d459efcbef3961c0227525d72fc67a735a73d730
Mayur <panchal.mayur2@gmail.com>
2014-05-04 11:41:38 +1200

    Initial commit

---
+++ app/src/main/java/com/myvictoria/app/SettingsFragment.java
@@ -0,0 +1,19 @@
+package com.myvictoria.app;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Mayur on 3/05/2014.
+ */
+public class SettingsFragment extends PreferenceFragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.preferences);
+    }
+}
